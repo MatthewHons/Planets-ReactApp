ea0b7a1176e99a47183b902b88ca60a08799af3d
Matthew Hons <devwithmatt@gmail.com>
2022-07-13 20:49:08 +0200

    Fix navbar

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import dataPlanet from "./../assets/data/db.json";
 import { Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
-import { useState } from "react";
-
 const MenuNav = () => {
   const [isMenuOpen, handleMenu] = useState(false);
   const handleCloseMenu = () => {
     handleMenu(false);
   };
-
   const handleStateChange = (state) => {
     handleMenu(state.isOpen);
   };
-
   return (
     <div>
       <div className="container-top">
         <h1>The planets</h1>
       </div>
-      <Menu width={375} isOpen={isMenuOpen} onStateChange={handleStateChange}>
+      <Menu width={375}  isOpen={isMenuOpen} onStateChange={handleStateChange}>
         <div className="container-list">
           {dataPlanet.planets.map((data, key) => {
             return (
               <div key={key} className="container-list-item">
-                <Link to={`/planet-${data.name}`} state={{ planet: data }}>
+                <Link to={`/planet-${data.name}`} state={{ planet: data }} onClick={() => handleCloseMenu()}>
                   <div
                     id="circle"
                     style={{ background: data.color }}
-                    onClick={() => handleCloseMenu()}
                   ></div>
                   {data.name}
                 </Link>
