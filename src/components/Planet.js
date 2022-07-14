7dc7d22f51bb17064d5ed1ba5b127432d19ecd17
Matthew Hons <devwithmatt@gmail.com>
2022-07-14 12:07:26 +0200

    Planets styling

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,26 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Planet = ({ planet }) => {
+ 
   return (
     <div>
+      <div className='navbar-border'>
+            <div className='navbar'>
+            <button style={{ borderBottom: '2px solid', padding:5, borderColor: planet.color  }}>overview</button>
+            </div>
+      </div>
       <img src={`./image/planet-${planet.name}.svg`} alt="{planet.name}" />
       <h2>{planet.name}</h2>
       <p className="text">{planet.overview.content}</p>
       <p className="source">Source : <a className="link" href={`${planet.overview.source}`}>Wikipedia</a></p>
       <table class="tg">
-    <tr>
-        <td class="tg-item"><span className="table-title">Rotation time</span> <span className="table-data">{planet.rotation}</span></td>
-    </tr>
-    <tr>
-        <td class="tg-item"><span className="table-title">Revolution time</span> <span className="table-data">{planet.revolution}</span></td>
-    </tr>
-    <tr>
-        <td class="tg-item"><span className="table-title">Radius</span> <span className="table-data">{planet.radius}</span></td>
-    </tr>
-    <tr>
-        <td class="tg-item"><span className="table-title">Average temp.</span> <span className="table-data">{planet.temperature}</span></td>
-    </tr>
-    </table>
+        <tr>
+            <td class="tg-item"><span className="table-title">Rotation time</span> <span className="table-data">{planet.rotation}</span></td>
+        </tr>
+        <tr>
+            <td class="tg-item"><span className="table-title">Revolution time</span> <span className="table-data">{planet.revolution}</span></td>
+        </tr>
+        <tr>
+            <td class="tg-item"><span className="table-title">Radius</span> <span className="table-data">{planet.radius}</span></td>
+        </tr>
+        <tr>
+            <td class="tg-item"><span className="table-title">Average temp.</span> <span className="table-data">{planet.temperature}</span></td>
+        </tr>
+      </table>
     </div>
   );
 };
